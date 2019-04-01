6d54696e529dc22d198f7b1f67b016338b7705ec
Anthony Hernandez <tony91139@gmail.com>
2019-04-01 07:25:37 -0700

    Add onClick handler to GifPreview

--- src/components/GifPreview/GifPreview.js
+++ src/components/GifPreview/GifPreview.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './GifPreview.css';
 import PropTypes from 'prop-types';
 
-function GifPreview({ title, url, height, width }) {
+function GifPreview({ height, id,  onClickHandler, title, url, width }) {
   const noImageToRender = !url;
   if (noImageToRender) return null;
   return (
-    <div className="GifPreview">
+    <div
+      className="GifPreview"
+      onClick={() => onClickHandler(id)}
+    >
       <img src={url} alt={title} height={height} width={width} />
     </div>
   );
 }
 
 GifPreview.propTypes = {
+  height: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired,
   width: PropTypes.string.isRequired,
 };
 
